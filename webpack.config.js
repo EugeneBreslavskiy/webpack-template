bdde9526276802399cf207840e63076b5ee28e69
Eugene Breslavskiy <eugenebr.92@gmail.com>
2020-12-14 10:29:39 +0300

    upload

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 9000,
+        open: true,
+        hot: true
+    }
+}
